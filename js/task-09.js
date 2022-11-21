34241a3bbe019f6457208185db259d60afeab181
DShpak27 <mr.shpak@gmail.com>
2022-11-21 21:16:28 +0200

    final fix

--- js/task-09.js
+++ js/task-09.js
@@ -3,6 +3,7 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 changeColorBtnRef.addEventListener("click", () => {
-    document.body.style.backgroundColor = getRandomHexColor();
-    document.querySelector(".color").textContent = getRandomHexColor();
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    document.querySelector(".color").textContent = randomColor;
 });
